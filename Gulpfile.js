fccd2492b49433daf39a68f3936e1edcb899ea4c
Eugene Dronov <dronov-ep@yandex.ru>
2022-01-15 09:12:17 +0300

    Итоговая база для построения Gulp билдов

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,9 @@
-var gulp = require("gulp");
+const {series, src, parallel, dest} = require("gulp");
 var Default = require("./gulpfiles/default.js")
 var Make = require("./gulpfiles/make.js")
 
 // команда gulp
 exports.default = Default.task;
-
 
 // команда gulp make
 exports.make = Make.task;
