99d00e8977d6a9fa3106b0edfa2dc5857b9b4e58
Eugene Dronov <dronov-ep@yandex.ru>
2022-01-30 23:35:58 +0300

    Максимально ускорил сборку стилей благодаря comman-line sass и плагину gulp-shell

--- gulpfiles/styles.js
+++ gulpfiles/styles.js
@@ -1,37 +1,10 @@
 'use strict';
 const {series, src, dest, watch, lastRun, parallel} = require("gulp");
-const sass = require('gulp-dart-sass');
-const cssnano = require('gulp-cssnano');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-
-function processSass() {
-    return src(['./src/styles/**/*.scss', './node_modules/bootstrap/dist/css/bootstrap.css'], {since: lastRun(processSass)})
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer({ overrideBrowserslist: ['>0.1%'] })]))
-        .pipe(dest('./obj/css/'));
-}
-
-function packStyles() {
-    return src('./obj/css/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(cssnano())
-        .pipe(concat('styles.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('./dist/css/'));
-}
+const shell = require('gulp-shell')
 
 
 
-function watchStyles() {
-    watch(
-        ['./src/styles/**/*.scss', './node_modules/bootstrap/dist/css/bootstrap.css'],
-        {ignoreInitial: false},
-        series(processSass, packStyles)
-    );
-}
 
-exports.task = series(processSass, packStyles);
-exports.watch = watchStyles;
+exports.task = shell.task('sass --color --style=compressed src/styles/:dist/css/');
+exports.watch = shell.task('sass --watch --color --style=compressed src/styles/:dist/css/');
+
