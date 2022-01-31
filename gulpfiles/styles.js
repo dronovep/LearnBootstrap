00c52a029a9073af9ed4a91fbf8623d71690a66d
Eugene Dronov <dronov-ep@yandex.ru>
2022-01-31 23:03:08 +0300

    Основные моменты с переменными в SASS

--- gulpfiles/styles.js
+++ gulpfiles/styles.js
@@ -3,7 +3,7 @@ const {series, src, dest, watch, lastRun, parallel} = require("gulp");
 const shell = require('gulp-shell')
 
 
-
+//ToDo Необходимо решить проблему с Autoprefixer
 
 exports.task = shell.task('sass --color --style=compressed src/styles/:dist/css/');
 exports.watch = shell.task('sass --watch --color --style=compressed src/styles/:dist/css/');
